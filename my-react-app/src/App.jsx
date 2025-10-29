4107aaf909e9a59e3c617e9af8a5642d43e49ed3
Sakshi Maulekhi <sakshimaulekhi84@gmail.com>
2025-10-29 18:41:58 +0530

    commit changes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/header';
+import Header from './components/Header';
 import Footer from './components/Footer';
 
 import HomePage from './pages/HomePage';
